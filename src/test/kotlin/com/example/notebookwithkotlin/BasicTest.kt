b14f8e0bbde9f5fc2e003cedbdb979b3445f0f03
haril <songkg7@gmail.com>
2022-04-01 22:57:14 +0900

    infix functions

--- src/test/kotlin/com/example/notebookwithkotlin/BasicTest.kt
+++ src/test/kotlin/com/example/notebookwithkotlin/BasicTest.kt
@@ -14,7 +14,29 @@ class BasicTest {
     }
 
     @Test
-    internal fun sum() {
+    internal fun infixFunctions() {
+        infix fun Int.times(str: String) = str.repeat(this)
+        println(2 times "Hello ")
 
+        val pair = "Ferrari" to "Katrina"
+        println(pair)
+
+        infix fun String.onto(other: String) = Pair(this, other)
+        val myPair = "McLaren" onto "Lucas"
+        println(myPair)
+
+        val sophia = Person("Sophia")
+        val claudia = Person("Claudia")
+
+        sophia likes claudia
+        sophia.likedPeople.forEach { println(it.name) }
+    }
+
+    class Person(val name: String) {
+        val likedPeople = mutableListOf<Person>()
+
+        infix fun likes(other: Person) {
+            likedPeople.add(other)
+        }
     }
 }
