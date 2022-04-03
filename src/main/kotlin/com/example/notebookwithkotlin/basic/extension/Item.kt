543199b2303dd713c1989d658ec0a0ee4f43fb94
kgsong <kyunggeun.song@seavantage.com>
2022-04-03 11:07:25 +0900

    Extension functions and properties

---
+++ src/main/kotlin/com/example/notebookwithkotlin/basic/extension/Item.kt
@@ -0,0 +1,3 @@
+package com.example.notebookwithkotlin.basic.extension
+
+data class Item(val name: String, val price: Float)
---
+++ src/main/kotlin/com/example/notebookwithkotlin/basic/extension/Order.kt
@@ -0,0 +1,3 @@
+package com.example.notebookwithkotlin.basic.extension
+
+data class Order(val items: Collection<Item>)
