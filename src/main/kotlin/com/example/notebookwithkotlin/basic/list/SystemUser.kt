678363367ebcce1b3954c7e933a0a5f9190ff9b8
kgsong <kyunggeun.song@seavantage.com>
2022-04-04 11:04:51 +0900

    List

---
+++ src/main/kotlin/com/example/notebookwithkotlin/basic/list/SystemUser.kt
@@ -0,0 +1,16 @@
+package com.example.notebookwithkotlin.basic.list
+
+class SystemUser {
+
+    val systemUsers: MutableList<Int> = mutableListOf(1, 2, 3)
+    val sudoers: List<Int> = systemUsers
+
+    fun addSystemUser(newUser: Int) {
+        systemUsers.add(newUser)
+    }
+
+    fun getSysSudoers(): List<Int> {
+        return sudoers
+    }
+
+}
---
+++ src/test/kotlin/com/example/notebookwithkotlin/basic/list/SystemUserTest.kt
@@ -0,0 +1,15 @@
+package com.example.notebookwithkotlin.basic.list
+
+import org.junit.jupiter.api.Test
+
+internal class SystemUserTest {
+
+    @Test
+    internal fun list() {
+        val systemUser = SystemUser()
+
+        systemUser.addSystemUser(4)
+        println("Tot sudoers: ${systemUser.getSysSudoers().size}")
+        systemUser.getSysSudoers().forEach { println("Some useful info on user $it") }
+    }
+}
