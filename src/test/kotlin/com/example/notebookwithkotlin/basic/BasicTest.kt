2180a7e420a16d500946563327316760ba8beecc
kgsong <kyunggeun.song@seavantage.com>
2022-04-05 14:35:51 +0900

    Map Element Access

--- src/test/kotlin/com/example/notebookwithkotlin/basic/BasicTest.kt
+++ src/test/kotlin/com/example/notebookwithkotlin/basic/BasicTest.kt
@@ -80,4 +80,28 @@ class BasicTest {
         println("mapWords = $mapWords")
         println("flatMapWords = $flatMapWords")
     }
+
+    @Test
+    internal fun mapElementAccess() {
+        val map = mapOf("key" to 42)
+
+        val value1 = map["key"]
+        val value2 = map["key2"]
+
+        val value3 = map.getValue("key")
+
+        val mapWithDefault = map.withDefault { it.length }
+        val value4 = mapWithDefault.getValue("key2")
+
+        try {
+            map.getValue("anotherKey")
+        } catch (e: NoSuchElementException) {
+            println("Message: $e")
+        }
+
+        println("value1 = $value1")
+        println("value2 = $value2")
+        println("value3 = $value3")
+        println("value4 = $value4")
+    }
 }
