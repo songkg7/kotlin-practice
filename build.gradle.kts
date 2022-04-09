cafa087e303809cff888e40f7a080c3a0bc80119
kgsong <kyunggeun.song@seavantage.com>
2022-04-10 00:56:16 +0900

    Kotest DescribeSpec 사용
    - 필요한 coroutines dependency 추가

--- build.gradle.kts
+++ build.gradle.kts
@@ -19,9 +19,11 @@ dependencies {
 	implementation("org.springframework.boot:spring-boot-starter")
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
 	implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+	implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core-jvm:1.6.0")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
 	testImplementation("io.kotest:kotest-runner-junit5:5.2.2")
 	testImplementation("io.kotest:kotest-assertions-core:5.2.2")
+	testImplementation("org.jetbrains.kotlinx:kotlinx-coroutines-test:1.6.0")
 }
 
 tasks.withType<KotlinCompile> {
