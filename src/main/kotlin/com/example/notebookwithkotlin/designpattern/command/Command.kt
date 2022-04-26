d70ef78b3b90842aaaca0ebc0ca36597f858ffd2
kgsong <kyunggeun.song@seavantage.com>
2022-04-26 17:11:29 +0900

    Command pattern

---
+++ src/main/kotlin/com/example/notebookwithkotlin/designpattern/command/Command.kt
@@ -0,0 +1,47 @@
+package com.example.notebookwithkotlin.designpattern.command
+
+interface Command {
+    fun execute()
+}
+
+class Button(private val command: Command) {
+    fun pressed() {
+        command.execute()
+    }
+}
+
+class Alarm {
+    fun alarmOn() {
+        println("Alarming!!")
+    }
+}
+
+class Lamp {
+    fun turnOn() {
+        println("turn on the lamp.")
+    }
+}
+
+class AlarmOnCommand : Command {
+    private val alarm = Alarm()
+
+    override fun execute() {
+        alarm.alarmOn()
+    }
+}
+
+class LampOnCommand : Command {
+    private val lamp = Lamp()
+
+    override fun execute() {
+        lamp.turnOn()
+    }
+}
+
+fun main() {
+    val alarmButton = Button(AlarmOnCommand())
+    val lampButton = Button(LampOnCommand())
+
+    alarmButton.pressed()
+    lampButton.pressed()
+}
