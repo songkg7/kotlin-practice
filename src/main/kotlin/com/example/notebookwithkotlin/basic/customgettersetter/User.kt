16ea8ba94f0b332af239ab229afe8adbaf670634
kgsong <kyunggeun.song@seavantage.com>
2022-05-23 11:08:35 +0900

    Custom getter setter

---
+++ src/main/kotlin/com/example/notebookwithkotlin/basic/customgettersetter/User.kt
@@ -0,0 +1,21 @@
+package com.example.notebookwithkotlin.basic.customgettersetter
+
+class User {
+    var name: String = "test"
+        get() = field.repeat(2)
+        set(value) {
+            field = value.uppercase()
+        }
+
+    var age: Int = 20
+        private set
+}
+
+fun main() {
+    val user = User()
+    println(user.name)
+
+    user.name = "change"
+    println(user.name)
+    println(user.age)
+}
