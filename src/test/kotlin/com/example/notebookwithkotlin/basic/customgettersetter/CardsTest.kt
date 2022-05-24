5a41a316e57ee35f12298c4f9b788aa1efed89e7
kgsong <kyunggeun.song@seavantage.com>
2022-05-24 10:08:09 +0900

    refactor: test description

--- src/test/kotlin/com/example/notebookwithkotlin/basic/customgettersetter/CardsTest.kt
+++ src/test/kotlin/com/example/notebookwithkotlin/basic/customgettersetter/CardsTest.kt
@@ -2,11 +2,13 @@ package com.example.notebookwithkotlin.basic.customgettersetter
 
 import io.kotest.matchers.collections.shouldContain
 import io.kotest.matchers.shouldBe
+import org.junit.jupiter.api.DisplayName
 import org.junit.jupiter.api.Test
 
 internal class CardsTest {
 
     @Test
+    @DisplayName("Backing properties 를 사용하여 객체 내부에서는 수정가능하지만 외부에서는 수정 불가능하게 한다.")
     internal fun test() {
         val cards = Cards()
 
