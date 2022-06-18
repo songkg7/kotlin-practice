f80b9e10af548aacb37ef4d0ab1378a6adf89e19
haril <songkg7@gmail.com>
2022-06-18 12:36:04 +0900

    chore: basic module 에서 spring 의존성 제거

--- basic/build.gradle.kts
+++ basic/build.gradle.kts
@@ -1,8 +1,5 @@
 plugins {
-    id("org.springframework.boot")
-    id("io.spring.dependency-management")
     kotlin("jvm")
-    kotlin("plugin.spring")
 }
 
 
@@ -11,12 +8,13 @@ repositories {
 }
 
 dependencies {
-    implementation("org.springframework.boot:spring-boot-starter")
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core-jvm:1.6.2")
+    testImplementation("org.assertj:assertj-core:3.23.1")
     testImplementation("io.kotest:kotest-runner-junit5:5.3.1")
     testImplementation("io.kotest:kotest-assertions-core:5.3.1")
     testImplementation("org.jetbrains.kotlinx:kotlinx-coroutines-test:1.6.2")
-    testImplementation("org.springframework.boot:spring-boot-starter-test")
+    testImplementation("org.junit.jupiter:junit-jupiter-api:5.8.2")
+    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.8.2")
 }
 
 tasks.withType<Test> {
