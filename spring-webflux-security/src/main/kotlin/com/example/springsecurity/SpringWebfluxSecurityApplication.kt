02f48ee336125c2fe01d4fc848fd3f06539bbb3e
haril <songkg7@gmail.com>
2022-08-30 21:29:31 +0900

    spring webflux security 모든 권한 제한 해제 상태로 시작

--- spring-webflux-security/src/main/kotlin/com/example/springsecurity/SpringWebfluxSecurityApplication.kt
+++ spring-webflux-security/src/main/kotlin/com/example/springsecurity/SpringWebfluxSecurityApplication.kt
@@ -4,8 +4,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
 @SpringBootApplication
-class SpringSecurityApplication
+class SpringWebfluxSecurityApplication
 
 fun main(args: Array<String>) {
-	runApplication<SpringSecurityApplication>(*args)
+	runApplication<SpringWebfluxSecurityApplication>(*args)
 }
