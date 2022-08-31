2304112bf3bd10b392ace7349a6276d1d7ac385b
kgsong <kyunggeun.song@seavantage.com>
2022-08-31 18:28:33 +0900

    Spring webflux stream 테스트

---
+++ spring-webflux/src/main/kotlin/com/example/springwebflux/controller/HelloController.kt
@@ -0,0 +1,31 @@
+package com.example.springwebflux.controller
+
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RestController
+import reactor.core.publisher.Flux
+import reactor.core.publisher.Mono
+import java.util.stream.Stream
+import kotlin.streams.asStream
+
+@RestController
+class HelloController {
+
+    @GetMapping("/mono")
+    fun helloMono() = Mono.just("hello")
+
+    @GetMapping("/flux")
+    fun helloFlux() = Flux.just("hello", "world")
+
+    @GetMapping("/flux/int")
+    fun intFlux(): Flux<Int> {
+        val intStream = Stream.of(1, 2, 3, 4, 5)
+
+        return Flux.fromStream(intStream)
+    }
+
+    @GetMapping("/flux/stream")
+    fun streamFlux(): Flux<Map<String, Int>> {
+        val stream = generateSequence(0) { it + 2 }.asStream()
+        return Flux.fromStream(stream.limit(5)).map { mapOf("value" to it) }
+    }
+}
