6dce834fb1ec614c173e225f98a85413f258cc6f
haril <songkg7@gmail.com>
2022-09-14 18:27:44 +0900

    webflux router stream

--- spring-webflux-router/src/main/kotlin/com/example/springwebfluxrouter/router/HelloHandler.kt
+++ spring-webflux-router/src/main/kotlin/com/example/springwebfluxrouter/router/HelloHandler.kt
@@ -1,9 +1,13 @@
 package com.example.springwebfluxrouter.router
 
+import org.springframework.core.ParameterizedTypeReference
 import org.springframework.http.MediaType
 import org.springframework.stereotype.Component
+import org.springframework.web.reactive.function.BodyExtractors.toFlux
 import org.springframework.web.reactive.function.BodyInserters.fromProducer
+import org.springframework.web.reactive.function.BodyInserters.fromPublisher
 import org.springframework.web.reactive.function.server.RequestPredicates.GET
+import org.springframework.web.reactive.function.server.RequestPredicates.POST
 import org.springframework.web.reactive.function.server.RouterFunctions.route
 import org.springframework.web.reactive.function.server.ServerRequest
 import org.springframework.web.reactive.function.server.ServerResponse
@@ -17,6 +21,8 @@ class HelloHandler {
 
     fun routes() = route(GET("/"), this::hello)
         .andRoute(GET("/stream"), this::stream)
+        .andRoute(POST("/echo"), this::echo)
+        .andRoute(POST("/steam"), this::postStream)
 
     fun hello(request: ServerRequest) =
         ok().body(Flux.just("Hello", "World!"), String::class.java)
@@ -27,6 +33,18 @@ class HelloHandler {
             .map { mapOf("value" to it) }
         return ok().contentType(MediaType.APPLICATION_NDJSON)
             .body(fromProducer(stream, Map::class.java))
+    }
+
+    fun echo(request: ServerRequest): Mono<ServerResponse> {
+        val body = request.bodyToMono(String::class.java).map { it.uppercase() }
+        return ok().body(body, String::class.java)
+    }
+
+    // NOTE: why object keyword is needed?
+    fun postStream(request: ServerRequest): Mono<ServerResponse> {
+        val body = request.body(toFlux(object : ParameterizedTypeReference<Map<String, Int>>() {}))
+        return ok().contentType(MediaType.TEXT_EVENT_STREAM)
+            .body(fromPublisher(body, object : ParameterizedTypeReference<Map<String, Int>>() {}))
     }
 
 }
