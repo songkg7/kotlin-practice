376e1a2c01e038e2e36470c3df2eb200bf1b1fa6
haril <songkg7@gmail.com>
2022-09-21 21:24:12 +0900

    boj 영수증

---
+++ algorithm/src/main/kotlin/bronze/boj25304/Solution.kt
@@ -0,0 +1,14 @@
+package bronze.boj25304
+
+// 영수증
+fun main() {
+    val totalPrice = readLine()!!.toInt()
+    val categories = readLine()!!.toInt()
+    var result = 0
+    repeat(categories) {
+        val split = readLine()!!.split(" ").map { it.toInt() }
+        result += split[0] * split[1]
+    }
+
+    println(if (result == totalPrice) "Yes" else "No")
+}
