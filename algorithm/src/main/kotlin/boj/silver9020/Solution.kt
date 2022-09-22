3b2874b09d4871527e2c1e9346a44ebfe1cfeff8
haril <songkg7@gmail.com>
2022-09-22 22:45:52 +0900

    BufferedReader 적용

--- algorithm/src/main/kotlin/boj/silver9020/Solution.kt
+++ algorithm/src/main/kotlin/boj/silver9020/Solution.kt
@@ -1,26 +1,37 @@
 package boj.silver9020
 
+import java.io.BufferedReader
+import java.io.BufferedWriter
+import java.io.InputStreamReader
+import java.io.OutputStreamWriter
 import kotlin.math.absoluteValue
 
 val primeNumber = primeNumbers()
 
 fun main() {
-    val t = readLine()!!.toInt()
+    val br = BufferedReader(InputStreamReader(System.`in`))
+    val bw = BufferedWriter(OutputStreamWriter(System.out))
+    val t = br.readLine().toInt()
     val list = mutableListOf<Int>()
     for (i in 1..t) {
-        list += readLine()!!.toInt()
+        list += br.readLine().toInt()
     }
-    list.forEach { printGoldbachPartition(it) }
+    list.forEach {
+        val result = goldbachPartition(it)
+        bw.write("${result.first} ${result.second}")
+        bw.newLine()
+    }
+    bw.flush()
+    bw.close()
 }
 
-fun printGoldbachPartition(n: Int) {
+fun goldbachPartition(n: Int): Pair<Int, Int> {
     // n 을 반으로 나눈 후, 각각의 숫자에서 가장 가까운 소수끼리 합을 구했을 때, n 과 일치하는 지 찾는다.
     // 4 ~ 10000 사이의 소수를 먼저 찾아놓고 실행
 
     // n 보다 작은 소수를 가져오고, 해당 소수 그룹에서 합이 n 과 같을 수 있는 모든 조합 생성
     // 조합에서 서로의 차이가 가장 작은 pair 를 선택
-    val position = primeNumber.indexOfFirst { it > n }
-    val numbers: List<Int> = when (position) {
+    val numbers: List<Int> = when (val position = primeNumber.indexOfFirst { it > n }) {
         -1 -> {
             primeNumber
         }
@@ -31,8 +42,7 @@ fun printGoldbachPartition(n: Int) {
 
     val combination = combination(numbers, n)
 
-    val result = lessDifference(combination)
-    println("${result.first} ${result.second}")
+    return lessDifference(combination)
 }
 
 fun primeNumbers(): List<Int> {
