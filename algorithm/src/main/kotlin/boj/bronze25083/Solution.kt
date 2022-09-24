f1af03fdefabe47b255a17e98e9f4b4a9f222b16
haril <songkg7@gmail.com>
2022-09-24 15:52:36 +0900

    boj 새싹

---
+++ algorithm/src/main/kotlin/boj/bronze25083/Solution.kt
@@ -0,0 +1,12 @@
+package boj.bronze25083
+
+// 새싹
+fun main() {
+    val string = "         ,r'\"7\n" +
+            "r`-_   ,'  ,/\n" +
+            " \\. \". L_r'\n" +
+            "   `~\\/\n" +
+            "      |\n" +
+            "      |"
+    println(string)
+}
