1ef576b596f00cbec3a85d9e4bdb91cceae9fcf6
haril <songkg7@gmail.com>
2022-10-01 17:20:47 +0900

    feat: elasticsearch 를 최신 버전에서 요구하는 방식으로 변경

--- spring-data-elasticsearch/src/main/kotlin/com/example/springdataelasticsearch/config/ElasticsearchConfig.kt
+++ spring-data-elasticsearch/src/main/kotlin/com/example/springdataelasticsearch/config/ElasticsearchConfig.kt
@@ -1,22 +1,23 @@
 package com.example.springdataelasticsearch.config
 
-import org.elasticsearch.client.RestHighLevelClient
+import co.elastic.clients.elasticsearch.ElasticsearchClient
+import co.elastic.clients.json.jackson.JacksonJsonpMapper
+import co.elastic.clients.transport.rest_client.RestClientTransport
+import org.apache.http.HttpHost
+import org.elasticsearch.client.RestClient
+import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
-import org.springframework.data.elasticsearch.client.ClientConfiguration
-import org.springframework.data.elasticsearch.client.RestClients
-import org.springframework.data.elasticsearch.config.AbstractElasticsearchConfiguration
 import org.springframework.data.elasticsearch.repository.config.EnableElasticsearchRepositories
 
 @Configuration
 @EnableElasticsearchRepositories
-class ElasticsearchConfig : AbstractElasticsearchConfiguration() {
+class ElasticsearchConfig {
 
-    override fun elasticsearchClient(): RestHighLevelClient {
-        val clientConfiguration = ClientConfiguration.builder()
-            .connectedTo("localhost:9200")
-            .build()
-
-        return RestClients.create(clientConfiguration).rest()
+    @Bean
+    fun elasticsearchClient(): ElasticsearchClient {
+        val restClient = RestClient.builder(HttpHost("localhost", 9200)).build()
+        val transport = RestClientTransport(restClient, JacksonJsonpMapper())
+        return ElasticsearchClient(transport)
     }
 
 }
