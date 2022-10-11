263ddf7d0c217d670d43288ccf9c708d68f451bb
haril <songkg7@gmail.com>
2022-10-11 20:29:01 +0900

    custom converter

--- design-pattern/src/main/kotlin/custom/Converter.kt
+++ design-pattern/src/main/kotlin/custom/Converter.kt
@@ -10,4 +10,10 @@ class ListConverter : Converter<String, List<String>> {
 
     override fun convert(s: String) = s.toList().map { it.toString() }
 
+    fun customConvert(): List<String> {
+        return stringConverter("test")
+    }
+
 }
+
+val stringConverter: (String) -> List<String> = { it.toList().map { c: Char -> c.toString() } }
