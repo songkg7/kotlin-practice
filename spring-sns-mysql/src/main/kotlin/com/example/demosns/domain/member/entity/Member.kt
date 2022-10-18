07cffc56bfc45275edc7b3ac46b0760ee015096a
haril <songkg7@gmail.com>
2022-10-18 18:48:26 +0900

    feat: unique member email

--- spring-sns-mysql/src/main/kotlin/com/example/demosns/domain/member/entity/Member.kt
+++ spring-sns-mysql/src/main/kotlin/com/example/demosns/domain/member/entity/Member.kt
@@ -2,6 +2,7 @@ package com.example.demosns.domain.member.entity
 
 import java.time.LocalDate
 import java.time.LocalDateTime
+import javax.persistence.Column
 import javax.persistence.Entity
 import javax.persistence.GeneratedValue
 import javax.persistence.Id
@@ -11,9 +12,14 @@ class Member(
     @Id
     @GeneratedValue
     val id: Long? = null,
+
     var nickname: String,
+
     val birthday: LocalDate,
+
+    @Column(unique = true)
     var email: String,
+
     val createAt: LocalDateTime = LocalDateTime.now(),
 ) {
     fun changeNickname(nickname: String) {
