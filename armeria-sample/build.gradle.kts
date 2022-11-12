b410880d45f67bc5a6b4625f1b17256dede3c1f8
haril <songkg7@gmail.com>
2022-11-12 15:59:36 +0900

    chore: update dependency

--- armeria-sample/build.gradle.kts
+++ armeria-sample/build.gradle.kts
@@ -11,10 +11,10 @@ repositories {
 }
 
 dependencies {
-    implementation("com.linecorp.armeria:armeria:1.20.1")
-    implementation("com.linecorp.armeria:armeria-kotlin:1.20.1")
+    implementation("com.linecorp.armeria:armeria:1.20.2")
+    implementation("com.linecorp.armeria:armeria-kotlin:1.20.2")
     implementation("ch.qos.logback:logback-classic:1.4.4")
-    implementation("io.github.microutils:kotlin-logging:3.0.3")
+    implementation("io.github.microutils:kotlin-logging:3.0.4")
 
     testImplementation("org.junit.jupiter:junit-jupiter-api:5.9.0")
     testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.9.0")
